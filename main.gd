7c4c6cde4aa918f064e9b298942d34b5dd76dd04
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-10-15 01:40:54 +0200

    More improvements

--- main.gd
+++ main.gd
@@ -9,49 +9,41 @@ var disable_controls = false
 func _ready():
 	Controller.set_max_force($force.value)
 	Controller.set_max_speed($speed.value)
-
-func _unhandled_input(event):
-	if not disable_controls and OS.get_ticks_msec() - click_cooldown > 50:
-		if event is InputEventMouseButton and event.is_pressed():
-			if event.button_index == BUTTON_RIGHT:
-				click_cooldown = OS.get_ticks_msec()
-				var obstacle = Obstacle.instance()
-				obstacle.initialize(get_global_mouse_position())
-				add_child(obstacle)
-		
-			if event.button_index == BUTTON_LEFT:
-				click_cooldown = OS.get_ticks_msec()
-				var e = Entity.instance()
-				e.initialize(get_global_mouse_position(), null)
-				add_child(e)
-
-	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().reload_current_scene()
+	$speed.connect("gui_input", self, "on_gui_input")
+	$speed.connect("mouse_exited", self, "on_gui_mouse_exited")
+	$force.connect("gui_input", self, "on_gui_input")
+	$force.connect("mouse_exited", self, "on_gui_mouse_exited")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	$Label.text = "FPS: %s" % String(Engine.get_frames_per_second())
-	$Label2.text = "Max speed: %s" % Controller.get_max_speed()
-	$Label3.text = "Max force: %s" % Controller.get_max_force()
-	
-#	if not disable_controls and OS.get_ticks_msec() - click_cooldown > 50:
-#		if Input.is_mouse_button_pressed(2):
-#			click_cooldown = OS.get_ticks_msec()
-#			var obstacle = Obstacle.instance()
-#			obstacle.initialize(get_global_mouse_position())
-#			add_child(obstacle)
-#
-#		if Input.is_mouse_button_pressed(1):
-#			click_cooldown = OS.get_ticks_msec()
-#			var e = Entity.instance()
-#			e.initialize(get_global_mouse_position(), null)
-#			add_child(e)
-#
-#	if Input.is_action_just_pressed("ui_accept"):
-#		get_tree().reload_current_scene()
+	$Label2.text = "Speed: %s" % Controller.get_max_speed()
+	$Label3.text = "Force: %s" % Controller.get_max_force()
+
+	if not disable_controls and OS.get_ticks_msec() - click_cooldown > 50:
+		if Input.is_mouse_button_pressed(2):
+			click_cooldown = OS.get_ticks_msec()
+			var obstacle = Obstacle.instance()
+			obstacle.initialize(get_global_mouse_position())
+			add_child(obstacle)
+
+		if Input.is_mouse_button_pressed(1):
+			click_cooldown = OS.get_ticks_msec()
+			var e = Entity.instance()
+			e.initialize(get_global_mouse_position(), null)
+			add_child(e)
+
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().reload_current_scene()
 		
 func _on_HSlider_value_changed(value):
 	Controller.set_max_speed(value)
 
 func _on_force_value_changed(value):
 	Controller.set_max_force(value)
+
+func on_gui_input(event):
+	disable_controls = true
+	
+func on_gui_mouse_exited():
+	disable_controls = false
